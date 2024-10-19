732167ceccb1b4269c17294cec13bd034a46febc
yangbingwx <2823462668@qq.com>
2024-10-19 18:16:58 +0800

    Add files via upload

---
+++ CookieDemo01.java
@@ -0,0 +1,27 @@
+package cookie;
+
+import com.example.class04cookiesession.HelloServlet;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+/**
+ * @author 28234
+ * @description: TODO
+ * @date 2024/10/19 13:50
+ */
+@WebServlet("/cookieDemo01")
+public class CookieDemo01 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Cookie cookie = new Cookie("username", "zhangsan");
+        resp.addCookie(cookie);
+        cookie.setMaxAge(60*60*24*7);
+
+    }
+}
